c2f96dfd0b8a634a685a4472a2057255fe912812
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-03-13 21:07:03 -0400

    ejercicio 1 arriba

---
+++ Ejercicio 1/Ejercicio 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Pasies
+{
+    static void Main()
+    {
+        Dictionary<string, string> paises = new Dictionary<string, string>
+        {   
+            {"Argentina","Buenos aires"},
+            {"Republica Dominicana","Santo Domingo"},
+            {"Haiti", "Puerto Principe"}
+        };
+        Console.WriteLine("Paises: ");
+        Console.WriteLine();
+        
+        foreach (var pais in paises)
+        {
+            Console.WriteLine(pais);
+        }
+    }
+
+
+}
+
+

142d421f7458a7f87190448a581a23bbb8ed6408
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-03-13 23:12:57 -0400

    Ejercicio 8

---
+++ Ejercicio_8/Ejercicio_8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Mostrar2
+{
+    static void Main()
+    {
+        Dictionary<String, String> Diccio = new Dictionary<String, String>
+        {
+            { "ID01", "Monitor Dell 24" },
+            { "ID02", "Teclado Mecánico RGB" },
+            { "ID03", "Mouse Inalámbrico" },
+            { "ID04", "Procesador Intel i7" },
+            { "ID05", "Memoria RAM 16GB" },
+            { "ID06", "Disco SSD 1TB" },
+            { "ID07", "Tarjeta Madre ATX" },
+            { "ID08", "Fuente de Poder 750W" },
+            { "ID09", "Gabinete Mid Tower" },
+            { "ID10", "Cable HDMI 2.1" }
+        };
+        
+
+        foreach(var Mos in Diccio)
+        {
+            Console.WriteLine("Keys: [" +Mos.Key+ "] Values: ["+ Mos.Value+"]");
+        }
+    }
+}

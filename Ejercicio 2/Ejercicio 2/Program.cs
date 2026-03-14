c6d68658723b2c90e170101b68f5fc65ada396f8
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-03-13 21:26:51 -0400

    ejercicio 3

--- Ejercicio 2/Ejercicio 2/Program.cs
+++ Ejercicio 2/Ejercicio 2/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-class Pasies
+class Productos
 {
     static void Main()
     {
---
+++ Ejercicio 3/Ejercicio 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Calificaciones
+{
+    static void Main()
+    {
+        Dictionary<string, int> notas = new Dictionary<string, int>
+        {
+            {"Luis Osvaldo",100},
+            {"Ana" ,90},
+            {"Jose", 85}
+        };
+
+        notas["Jose"] = 80;
+      
+        Console.WriteLine($"Las notas del estudiante= " + notas["Jose"]);
+
+
+    }
+
+
+}

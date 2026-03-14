8c98665f66f330a5779c515c4e9bd414f58974ea
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-03-13 23:24:43 -0400

    Ejercicio 9

---
+++ Ejercicio 9/Ejercicio 9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class sumarValues
+{
+    static void Main()
+    {
+        Dictionary<String, Double> Suma = new Dictionary<String, Double>
+        {
+            { "Laptop", 850.50 },
+            { "Mouse", 25.99 },
+            { "Teclado", 45.00 },
+            { "Monitor", 199.99 },
+            { "Disco Duro", 120.75 },
+            { "Memoria RAM", 89.90 },
+            { "Procesador", 320.00 },
+            { "Tarjeta de Video", 450.25 },
+            { "Fuente Poder", 75.50 },
+            { "Gabinete", 60.00 }
+        };
+        Console.Write("La suma de todos los valores es = ");
+        Console.WriteLine(Suma.Values.Sum());
+
+    }
+}

5c77ff252ba2d66f4362e9967ea3890579be7008
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-03-13 21:36:34 -0400

    Ejercicio 4

---
+++ Ejercicio 4/Ejercicio 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Eliminacion
+{
+    static void Main()
+    {
+        Dictionary<string, int> Eli = new Dictionary<string, int>
+        {
+            {"Maria",100},
+            {"Angela" ,90},
+            {"Redobledo", 85}
+        };
+        Eli.Remove("Angela");
+
+        foreach(var Elimi in Eli) { Console.WriteLine(Elimi); }
+
+        
+
+
+
+    }
+
+
+}

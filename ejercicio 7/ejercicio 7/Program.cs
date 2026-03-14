45421a08d8ed5af85ea65093c821c4954e9e101d
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-03-13 22:33:19 -0400

    Ejercicio 7

---
+++ ejercicio 7/ejercicio 7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class NombresApellidos
+{
+    static void Main()
+    {
+        Dictionary<String, String> NoApe = new Dictionary<String, String>
+        {
+            {"Luis", "Peralta"},
+            {"Redobledo", "Sanchez"},
+            {"Jose","Ramirez"},
+        };
+
+        if (NoApe.ContainsKey("Luis"))
+        {
+            Console.WriteLine("La llave esta ahi");
+        }
+        if (NoApe.ContainsKey("Maria"))
+        {
+            Console.WriteLine("La llave esta ahi");
+        }
+        else { Console.WriteLine("La llave no esta ahi"); }
+
+
+
+    }
+}

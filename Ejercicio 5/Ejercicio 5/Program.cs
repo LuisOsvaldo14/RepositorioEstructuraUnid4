dcfa2c78f8f9e3cf8ab6508d272afa698559c6b3
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-03-13 21:42:33 -0400

    Ejercicio 5

---
+++ Ejercicio 5/Ejercicio 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Muestra
+{
+    static void Main()
+    {
+        Dictionary<string, Double> Mostrar = new Dictionary<string, Double>
+        {
+            {"Maria",100},
+            {"Angela" ,90},
+            {"Redobledo", 85},
+            {"Romeo", 87.44},
+            {"Justin", 55.432 }
+        };
+        
+
+        foreach (var Do in Mostrar) { Console.WriteLine("Mostrando llaves: "+ Do.Key); }
+
+
+
+
+
+    }
+
+
+}

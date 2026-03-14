9595d17e29190e29a23d9847d40415bcfbe8a690
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-03-13 21:13:48 -0400

    ejercicio 2

---
+++ Ejercicio 2/Ejercicio 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Pasies
+{
+    static void Main()
+    {
+        Dictionary<string, int> Productos = new Dictionary<string, int>
+        {
+            {"Jabon",120},
+            {"Pollo Frito" ,400},
+            {"Coca Cola", 50}
+        };
+        Console.WriteLine("El precio de la Coca Cola es:" + Productos["Coca Cola"]);
+        Console.WriteLine();
+
+
+    }
+
+
+}

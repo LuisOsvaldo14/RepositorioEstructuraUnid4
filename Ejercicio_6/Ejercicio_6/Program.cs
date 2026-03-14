6f09f7873e6cf20b7bc0d98939663e0752ec439a
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-03-13 21:58:59 -0400

    Ejercicio 6

---
+++ Ejercicio_6/Ejercicio_6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class NombresApellidos
+{
+    static void Main()
+    {
+        Dictionary<String, String> NoApe = new Dictionary<String, String>
+        {
+            {"Luis", "Peralta"},
+            {"Redobledo", "Sanchez"},
+            {"Jose","Ramirez"},
+            {"Marie","Pie" },
+            {"Rafelin","Romeo" }
+        };
+
+        foreach(var Mos in NoApe) { Console.WriteLine("Mostrando los valores: " + Mos.Value); }
+
+
+    }
+}
+
